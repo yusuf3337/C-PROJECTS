2abfbc84332cf2ecc3bf961e1785fce97e5e7959
Yusuf Çepni <81266195+yusuf3337@users.noreply.github.com>
2022-09-09 18:05:27 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int yas = 0;
+	printf("Yasinizi Giriniz :\n");
+	
+	scanf("%d",&yas);
+
+	//EFFECT
+	printf("HESAPLANIYOR........................................>TAMAMLANDI!\n\n\n\n\n");
+	
+	
+	if (yas < 18){
+	
+	printf("Oy kullanamazsiniz");
+}
+	
+	else{
+		printf("Oy Kullanabilirsiniz");
+	}
+	
+																																																				//YUSUF ÇEPNÝ -@yusuf3337
+	
+	return 0;
+}
